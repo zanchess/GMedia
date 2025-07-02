610248ec625838756cf44ca5ddb05f16c7ed90ca
Lashkevich Aliaksandr <a.lashkevich93@gmail.com>
2025-07-02 10:37:53 +0300

    feat: init package json and docker and enums

---
+++ src/enum/error-code.enum.ts
@@ -0,0 +1,6 @@
+export enum ErrorCode {
+  SUCCESS = 200,
+  CREATED = 201,
+  NOT_FOUND = 404,
+  INTERNAL_SERVER_ERROR = 500,
+}
---
+++ src/enum/rabbitmq-value.enum.ts
@@ -0,0 +1,5 @@
+export enum RABBITMQ_VALUE {
+  TASK_EXCHANGE = 'task.exchange',
+  QUEUE = 'task.actions',
+  ROUTING_KEY = 'task.action',
+}
